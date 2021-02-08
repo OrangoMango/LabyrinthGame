e32a3b395c695c97c45696bb9d45de8858f73f65
Orango Mango <orapama06@gmail.com>
2021-02-08 12:18:59 +0100

    Add files via upload

--- app/run.sh
+++ app/run.sh
@@ -1,5 +1,4 @@
-export FX_PATH=/usr/share/openjfx/lib
 cd src
-javac --module-path $FX_PATH --add-modules javafx.controls -d ../bin com/orangomango/labyrinth/LabyrinthMain.java
+javac -d ../bin com/orangomango/labyrinth/LabyrinthMain.java
 cd ../bin
-java --module-path $FX_PATH --add-modules javafx.controls com.orangomango.labyrinth.LabyrinthMain
+java com.orangomango.labyrinth.LabyrinthMain
